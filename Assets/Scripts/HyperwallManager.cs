7ad36dc1a99e83702a5c40a69cdf8f70b1872bff
Phil Cherner <pcherner@mit.edu>
2023-04-04 12:00:11 -0400

    Added map controls and water

--- Assets/Scripts/HyperwallManager.cs
+++ Assets/Scripts/HyperwallManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HyperwallManager : MonoBehaviour
 {
+    public bool isMapOpen = false;
+    public GameObject contents;
+    public GameObject mapContainer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,21 @@ public class HyperwallManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMapOpen)
+        {
+            contents.SetActive(false);
+            mapContainer.transform.localPosition = new Vector3(0, 0, 4.6f);
+            mapContainer.transform.localScale = new Vector3(1, 1, 1.97f);
+        } else
+        {
+            mapContainer.transform.localPosition = new Vector3(0.76f, 5.43f, -1.55f);
+            mapContainer.transform.localScale = new Vector3(1, 0.51f, 0.94f);
+            contents.SetActive(true);
+        }
+    }
+
+    public void toggleMap()
+    {
+        isMapOpen = !isMapOpen;
     }
 }
